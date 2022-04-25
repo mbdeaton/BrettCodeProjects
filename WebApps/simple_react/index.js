ec3f7f162df174d7c0d71d5fabb3e55416a749f7
Brett Deaton <brett.deaton@gmail.com>
2022-04-24 18:15:38 -0700

    Change indents to 2 spaces

--- WebApps/simple_react/index.js
+++ WebApps/simple_react/index.js
@@ -1,28 +1,28 @@
 let createElement = React.createElement
-            
+      
 let rootElement =
-    createElement('div', {},
-        createElement('hi', {}, "Contacts"),
-        createElement('ul', {id: 'nav'},
-            createElement(
-                'li',
-                {},
-                createElement(
-                    'a',
-                    { href: 'mailto:brett.deaton@gmail.com' },
-                    "Brett Deaton"
-                ),
-            ),
-            createElement(
-                'li',
-                {},
-                createElement(
-                    'a',
-                    { href: 'mailto:darcideaton@gmail.com' },
-                    "Darci Deaton"
-                )
-            )
+  createElement('div', {},
+    createElement('hi', {}, "Contacts"),
+    createElement('ul', {id: 'nav'},
+      createElement(
+        'li',
+        {},
+        createElement(
+          'a',
+          { href: 'mailto:brett.deaton@gmail.com' },
+          "Brett Deaton"
+        ),
+      ),
+      createElement(
+        'li',
+        {},
+        createElement(
+          'a',
+          { href: 'mailto:darcideaton@gmail.com' },
+          "Darci Deaton"
         )
+      )
     )
+  )
 let domNode = document.getElementById('root')
 ReactDOM.render(rootElement, domNode)
