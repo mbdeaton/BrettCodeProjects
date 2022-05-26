37f7e53963859dc018d9bb5abb681240f8f4b637
Brett Deaton <brett.deaton@gmail.com>
2022-05-25 23:41:13 -0700

    Convert canvas to svg

--- WebApps/musical_log_spiral/scripts/log-spiral.js
+++ WebApps/musical_log_spiral/scripts/log-spiral.js
@@ -1,39 +1,38 @@
-let c = document.getElementById("spiral-canvas");
-let ctx = c.getContext("2d");
-let scale = c.clientWidth;
+const c = document.getElementById("spiral-canvas");
+const scale = 100;
 const radius = 0.9*scale/2;
+c.setAttribute("viewBox", `${-scale/2} ${-scale/2} ${scale} ${scale}`)
+c.innerHTML += `<circle cx='0' cy='0' r='${radius}' fill='lightblue'/>`;
 
 function drawNoteLines() {
   const noteNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
   const angleStep = 2*Math.PI / 12;
-
+  
   for (let i = 0; i < noteNames.length; i++) {
     const angle = i*angleStep + Math.PI/2;
-    const x = -radius*Math.cos(angle)+scale/2;
-    const y = -radius*Math.sin(angle)+scale/2;
-    ctx.moveTo(scale/2, scale/2);
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.fillText(noteNames[i], x, y);
+    const x = -radius*Math.cos(angle);
+    const y = -radius*Math.sin(angle);
+    c.innerHTML += `<line class='note-line' x2='${x}' y2='${y}' stroke='whitesmoke' stroke-width='0.2px'/>`;
+    c.innerHTML += `<text x=${1.05*x} y=${1.05*y} fill='lightblue' font-size='0.2rem' text-anchor='middle' dominant-baseline='middle'>${noteNames[i]}</text>`;
   }
 }
 
 function drawSpiral() {
   const b = Math.LN2 / 2 / Math.PI;
-  const centsPerStep = 50;
-  const numOctaves = 5;
+  const centsPerStep = 25;
+  const numOctaves = 8;
   const numSteps = numOctaves * 1200 / centsPerStep;
   const angleStep = 2*Math.PI * centsPerStep / 1200;
-  const radiusStart = 10;
-  ctx.moveTo(scale/2, scale/2)
+  const radiusStart = scale/400;
+  c.innerHTML += `<path id='spiral' d='M 0 ${-radiusStart}' stroke='gray' stroke-width='0.2px' fill='none'/>`;
+  const p = document.getElementById("spiral");
 
-  for (let i = 0; i < numSteps; i++) {
+  for (let i=1; i < numSteps+1; i++) {
     const angle = i*angleStep + Math.PI/2;
     const radius = radiusStart * Math.exp(b*(angle-Math.PI/2));
-    const x = radius*Math.cos(angle)+scale/2;
-    const y = -radius*Math.sin(angle)+scale/2;
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    const x = radius*Math.cos(angle);
+    const y = -radius*Math.sin(angle);
+    p.setAttribute('d', p.getAttribute('d') + ` ${x} ${y}`);
   }
 }
 
