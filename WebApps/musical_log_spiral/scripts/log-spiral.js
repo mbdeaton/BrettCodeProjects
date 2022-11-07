c893c5f12fa58ceda9d36f7e97ab370ba73ce42c
Brett Deaton <brett.deaton@gmail.com>
2022-11-06 21:23:15 -0800

    Refactor, and add functionality
    * drop a dot on the note
    * play a representative note

--- WebApps/musical_log_spiral/scripts/log-spiral.js
+++ WebApps/musical_log_spiral/scripts/log-spiral.js
@@ -1,21 +1,147 @@
-const c = document.getElementById("spiral-canvas");
-const svg = document.querySelector("svg");
-const svgns = "http://www.w3.org/2000/svg";
-const scaleSvg = 100;
-const radius = (0.9 * scaleSvg) / 2;
-c.setAttribute(
-  "viewBox",
-  `${-scaleSvg / 2} ${-scaleSvg / 2} ${scaleSvg} ${scaleSvg}`
-);
-let mainCirc = document.createElementNS(svgns, "circle");
-mainCirc.setAttribute("cx", "0");
-mainCirc.setAttribute("cy", "0");
-mainCirc.setAttribute("r", `${radius}`);
-mainCirc.setAttribute("fill", "lightblue");
-svg.appendChild(mainCirc);
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// Tools to draw a logarithmic spiral representing all possible
+// notes as an SVG. Prototype by Brett Deaton - Fall 2022
 
-svg.addEventListener("click", playNote);
+// Draw the background disc enclosing all the notes within radius.
+function drawMainDisc(radius) {
+  let mainCirc = document.createElementNS(svgns, "circle");
+  mainCirc.setAttribute("cx", "0");
+  mainCirc.setAttribute("cy", "0");
+  mainCirc.setAttribute("r", `${radiusFundamental}`);
+  mainCirc.setAttribute("fill", "lightblue");
+  svg.appendChild(mainCirc);
+}
+
+// Draw 12 radii representing the 12 notes of the chromatic scale.
+function drawNoteLines() {
+  const noteNames = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
+  const angleStep = (2 * Math.PI) / 12;
+
+  for (let i = 0; i < noteNames.length; i++) {
+    const angle = i * angleStep + Math.PI / 2;
+    const x = -radiusFundamental * Math.cos(angle);
+    const y = -radiusFundamental * Math.sin(angle);
+
+    let noteLine = document.createElementNS(svgns, "line");
+    noteLine.classList.add("note-line");
+    noteLine.setAttribute("x2", `${x}`);
+    noteLine.setAttribute("y2", `${y}`);
+    noteLine.setAttribute("stroke", "whitesmoke");
+    noteLine.setAttribute("stroke-width", "0.2px");
+
+    let noteLabel = document.createElementNS(svgns, "text");
+    noteLabel.setAttribute("x", `${1.05 * x}`);
+    noteLabel.setAttribute("y", `${1.05 * y}`);
+    noteLabel.setAttribute("fill", "lightblue");
+    noteLabel.setAttribute("font-size", "0.2rem");
+    noteLabel.setAttribute("text-anchor", "middle");
+    noteLabel.setAttribute("dominant-baseline", "middle");
+    noteLabel.innerHTML = noteNames[i];
+
+    svg.appendChild(noteLine);
+    svg.appendChild(noteLabel);
+  }
+}
+
+// Draw the logarithmic spiral representing all frequencies.
+//   r = r_0 exp(b(theta-pi/2))
+// where
+//   r_0    the fundamental radius, representing the key center
+//   r      coordinate radius in SVG units, proportional the wavelength
+//   theta  counter-clockwise angle from positive y-axis (in the math
+//          convention, going up the page), with larger theta representing
+//          lower notes.
+//   b      rate constant forcing the spiral to double in radius
+//          every full rotation
+// Details on this equation in `notes/notes-log_spiral-?.png`.
+function drawSpiral() {
+  const numOctaves = 8; // how many octaves to display?
+  const centsPerStep = 25; // decrease for better spiral resolution
+  const b = Math.LN2 / 2 / Math.PI;
+  const centsPerOctave = 1200;
+  const numSteps = (numOctaves * centsPerOctave) / centsPerStep;
+  const angleStep = (2 * Math.PI * centsPerStep) / centsPerOctave;
+  const radiusStart = radiusFundamental / 2 ** numOctaves;
+  let spiral = document.createElementNS(svgns, "path");
+  spiral.id = "spiral";
+  spiral.setAttribute("stroke", "gray");
+  spiral.setAttribute("stroke-width", "0.2px");
+  spiral.setAttribute("fill", "none");
+
+  const pointPairsXY = [0, -radiusStart];
+  for (let i = 1; i < numSteps + 1; i++) {
+    const angle = i * angleStep + Math.PI / 2;
+    const radius = radiusStart * Math.exp(b * (angle - Math.PI / 2));
+    const x = radius * Math.cos(angle);
+    const y = -radius * Math.sin(angle);
+    pointPairsXY.push(x, y);
+  }
+
+  spiral.setAttribute("d", "M" + pointPairsXY.join(" "));
+  svg.appendChild(spiral);
+}
+
+// Play a note of given frequency (Hz) and duration (s).
+function playNote(frequency, duration = 0.5) {
+  // create Oscillator node
+  const oscillator = audioCtx.createOscillator();
+
+  oscillator.frequency.value = frequency;
+  oscillator.connect(audioCtx.destination);
+  oscillator.start(0);
+  oscillator.stop(audioCtx.currentTime + duration);
+}
+
+// Return the unique x,y pair on the spiral having radius.
+//   theta = log(r/r_0)/b + c
+// where
+//   theta  counter-clockwise angle from positive y-axis (in the math
+//          convention, going up the page), with larger theta representing
+//          lower notes.
+//   r      coordinate radius in SVG units, proportional the wavelength
+//   r_0    the fundamental radius, representing the key center
+//   b      rate constant forcing the spiral to double in radius
+//          every full rotation
+// Details on this equation in `notes-log_spiral-?.png`.
+function coordsFromRadius(radius) {
+  const b = 1 / Math.LOG2E / 2 / Math.PI;
+  const c = Math.PI / 2;
+  const theta = Math.log(radius / radiusFundamental) / b + c;
+  return [radius * Math.cos(theta), -radius * Math.sin(theta)]; // [x,y]
+}
+
+// Given a radius, return the corresponding frequency, or vice versa.
+// The physics relationship is frequency = soundspeed/2pi/radius
+function frequencyFromRadius(radius) {
+  return scaleSpeed / radius; // in Hz
+}
+function radiusFromFrequency(frequency) {
+  return scaleSpeed / frequency; // in SVG units
+}
+
+// Draw a dot on the spiral at the given radius.
+function drawNote(radius) {
+  const radiusDot = radiusFundamental / 50;
+  const [x, y] = coordsFromRadius(radius);
+  let dot = document.createElementNS(svgns, "circle");
+  dot.setAttribute("cx", `${x}`);
+  dot.setAttribute("cy", `${y}`);
+  dot.setAttribute("r", `${radiusDot}`);
+  dot.setAttribute("fill", "orange");
+  svg.appendChild(dot);
+}
 
 // Return the matrix M that translates DOM coords x to SVG coords x' using
 // x' = x.matrixTransform(M)
@@ -46,107 +172,38 @@ function getDomMatrix() {
   return matrix;
 }
 
-// Play a note and place a dot representing the note on the svg.
-function playNote(evnt) {
+function handleClick(evnt) {
   const matrix = getDomMatrix();
   const point = new DOMPoint(evnt.clientX, evnt.clientY);
   // console.log(`point: ${point.x} ${point.y}`); // DEBUG
   const pointSvg = point.matrixTransform(matrix);
   // console.log(`pointSvg: ${pointSvg.x} ${pointSvg.y}`); // DEBUG
 
-  const frequency = 261.626; // middle C, i.e. C4
-  const duration = 0.5;
-
-  // create Oscillator node
-  const oscillator = audioCtx.createOscillator();
-
-  oscillator.frequency.value = frequency;
-  oscillator.connect(audioCtx.destination);
-  oscillator.start(0);
-  oscillator.stop(audioCtx.currentTime + duration);
+  const x = pointSvg.x;
+  const y = pointSvg.y;
+  const r = Math.sqrt(x ** 2 + y ** 2);
+  drawNote(r);
+  playNote(frequencyFromRadius(r));
 }
 
-// Draw the 12 radii representing the 12 notes of the
-// chromatic scale.
-function drawNoteLines() {
-  const noteNames = [
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-  ];
-  const angleStep = (2 * Math.PI) / 12;
+// Main driver
+const c = document.getElementById("spiral-canvas");
+const svg = document.querySelector("svg");
+const svgns = "http://www.w3.org/2000/svg";
+const scaleSvg = 100; // SVG unit size of the viewBox
+const radiusFundamental = (0.9 * scaleSvg) / 2;
+c.setAttribute(
+  "viewBox",
+  `${-scaleSvg / 2} ${-scaleSvg / 2} ${scaleSvg} ${scaleSvg}`
+);
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+svg.addEventListener("click", handleClick);
 
-  for (let i = 0; i < noteNames.length; i++) {
-    const angle = i * angleStep + Math.PI / 2;
-    const x = -radius * Math.cos(angle);
-    const y = -radius * Math.sin(angle);
+// Set scale between lengths and frequencies
+const frequencyC4 = 261.626; // middle C, i.e. C4
+const frequencyFundamental = frequencyC4 / 2; // lowest note
+const scaleSpeed = frequencyFundamental * radiusFundamental;
 
-    let noteLine = document.createElementNS(svgns, "line");
-    noteLine.classList.add("note-line");
-    noteLine.setAttribute("x2", `${x}`);
-    noteLine.setAttribute("y2", `${y}`);
-    noteLine.setAttribute("stroke", "whitesmoke");
-    noteLine.setAttribute("stroke-width", "0.2px");
-
-    let noteLabel = document.createElementNS(svgns, "text");
-    noteLabel.setAttribute("x", `${1.05 * x}`);
-    noteLabel.setAttribute("y", `${1.05 * y}`);
-    noteLabel.setAttribute("fill", "lightblue");
-    noteLabel.setAttribute("font-size", "0.2rem");
-    noteLabel.setAttribute("text-anchor", "middle");
-    noteLabel.setAttribute("dominant-baseline", "middle");
-    noteLabel.innerHTML = noteNames[i];
-
-    svg.appendChild(noteLine);
-    svg.appendChild(noteLabel);
-  }
-}
-
-// Draw the logarithmic spiral representing all frequencies.
-//   r = r_0 exp(b(theta-pi/2))
-// where
-//   r      coordinate radius, proportional the wavelength
-//   theta  counter-clockwise angle from positive y-axis, with
-//          larger theta representing lower notes
-//   b      rate constant forcing the spiral to double in radius
-//          every full rotation
-//   r_0    the principle radius, representing the key center
-// Details on this equation in `notes/notes-log_spiral-?.png`.
-function drawSpiral() {
-  const b = Math.LN2 / 2 / Math.PI;
-  const centsPerStep = 25; // decrease for better spiral resolution
-  const numOctaves = 8; // how many octaves to display?
-  const centsPerOctave = 1200;
-  const numSteps = (numOctaves * centsPerOctave) / centsPerStep;
-  const angleStep = (2 * Math.PI * centsPerStep) / centsPerOctave;
-  const radiusStart = scaleSvg / 400;
-  let spiral = document.createElementNS(svgns, "path");
-  spiral.id = "spiral";
-  spiral.setAttribute("stroke", "gray");
-  spiral.setAttribute("stroke-width", "0.2px");
-  spiral.setAttribute("fill", "none");
-
-  const pointPairsXY = [0, -radiusStart];
-  for (let i = 1; i < numSteps + 1; i++) {
-    const angle = i * angleStep + Math.PI / 2;
-    const radius = radiusStart * Math.exp(b * (angle - Math.PI / 2));
-    const x = radius * Math.cos(angle);
-    const y = -radius * Math.sin(angle);
-    pointPairsXY.push(x, y);
-  }
-
-  spiral.setAttribute("d", "M" + pointPairsXY.join(" "));
-  svg.appendChild(spiral);
-}
-
+drawMainDisc();
 drawNoteLines();
 drawSpiral();
