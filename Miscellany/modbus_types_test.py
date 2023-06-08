514682415a38ba873c1f6153d8e57e9f301e0d30
Brett Deaton <brett_deaton@selinc.com>
2023-06-07 17:01:52 -0700

    Make slight improvements to modbus_types

--- Miscellany/modbus_types_test.py
+++ Miscellany/modbus_types_test.py
@@ -1,15 +1,22 @@
+"""Test Registers in modbus_types.
+
+Example usage:
+    $ python3 -m unittest -v modbus_types_test.py
+"""
 import unittest
 from modbus_types import Registers
 
-# Execute with python3 -m unittest -v modbus_types_test.py
-
 
 class TestPairs(unittest.TestCase):
+    """Test two registers interpreted as different types"""
+
     def setUpClass(self):
+        """Create two 2-byte test registers"""
         self.reg0 = Registers(0x1234)
-        self.reg2 = Registers(0xabcd)
+        self.reg1 = Registers(0xabcd)
 
     def test_uint16(self):
+        """Interpret registers as 16-bit unsigned integers"""
         defs = {"rep": "int", "sig": "+", "bit": "16"}
         self.reg0.set_typedef(defs)
         self.assertEqual(self.reg0.to_data(), 4660)
